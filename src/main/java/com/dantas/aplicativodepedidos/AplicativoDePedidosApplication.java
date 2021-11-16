d9fd2641de3fa9d70f381db9d7739d26f3491a9c
Rafael Dantas <developer.dantas@outlook.com>
2021-11-16 18:24:24 -0300

    Created Project

---
+++ src/main/java/com/dantas/aplicativodepedidos/AplicativoDePedidosApplication.java
@@ -0,0 +1,13 @@
+package com.dantas.aplicativodepedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicativoDePedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicativoDePedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dantas/aplicativodepedidos/AplicativoDePedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dantas.aplicativodepedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicativoDePedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
