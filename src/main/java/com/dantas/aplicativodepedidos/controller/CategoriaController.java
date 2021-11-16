f046b8433d4294882b4aad19c43873a746cf404b
Rafael Dantas <developer.dantas@outlook.com>
2021-11-16 18:53:49 -0300

    Criando classe de dominio Categoria

--- src/main/java/com/dantas/aplicativodepedidos/controller/CategoriaController.java
+++ src/main/java/com/dantas/aplicativodepedidos/controller/CategoriaController.java
@@ -1,15 +1,26 @@
 package com.dantas.aplicativodepedidos.controller;
 
+import com.dantas.aplicativodepedidos.model.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaController {
 
     @GetMapping
-    public String listar() {
-        return "Hello, World";
+    public List<Categoria> listar() {
+        Categoria categoria1 = new Categoria(1, "Informática");
+        Categoria categoria2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(categoria1);
+        lista.add(categoria2);
+
+        return lista;
     }
 }
